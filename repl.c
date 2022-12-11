4edc58e6e75942c08f9090736e8ee0e2f885eee2
stacksmith <fpgasm@apple2.x10.mx>
2022-12-11 12:16:48 -0800

    cleaned up help, initial load, edit

--- repl.c
+++ repl.c
@@ -61,7 +61,7 @@ sElf* rebuild(char* name,U32 need_function){
   pks_dump_protos();
   char buf[256];
   sprintf(buf,"cd sys; ./build.sh %s",name);
-  puts(buf);
+   //puts(buf);
   int ret = system(buf);
   if(ret){
     printf("OS shellout to compiler failed! Build Error %d\n",ret);
@@ -168,12 +168,41 @@ void repl_list(char* p){
 void repl_words(char* p){
   pk_dump((sSym*)(U64)SRCH_LIST);
 }
+char* helpstr =
+  "built-in commands:\n\
+ bye<cr>           exit\n\
+ cc<cr>            compile sys/unit.c; please edit sys/body.c first\n\
+ dump XXXXXXXX<cr> hexdump data at hex address\n\
+ edit <name><cr>   put the source of symbol into sys/body.c\n\
+ help<cr>          displays this message\n\
+ list <name><cr>   show the source of the symbol\n\
+ load<cr>          load the image file\n\
+ save<cr>          save the image in image/image.dat file\n\
+ sys<cr>           show system statistics\n\
+ words<cr>         show the symbol table\n\
+\n\
+puts(\"hello\");     execute any stdlib or your own function\n\
+\n\
+The entire libc is available; <stdio.h> and <stdlib.h> are always included; \n\
+other headers may need to be included in each unit, add to /sys/types.h if \n\
+you intend to use a header or type a lot.\n\
+\n\
+Each edit/compilation unit must have exactly one global symbol - one data \n\
+object or one function.  Make sure to add declarations for global data; \n\
+your function prototypes will be automatically generated.\n\n\
+If you use emacs, keep a buffer with sys/body.c, and turn on \n\
+auto-revert-mode.  Now when you type in 'edit foo<cr>', the source of foo \n\
+will appear in the buffer.  After making changes, enter `cc` to compile and \n\
+integrate the changes.  Or edit sys/body.c, save, and cc to compile.\n\
+\n\
+You can call any compiled function from the command line.\n";
+
+
+
+
 
 void repl_help(char*p){
-  printf("commands:\n sys  words  bye  edit <name>  list <name>\n");
-  printf(".<expr><cr> to execute an expression.  For instance:\n");
-  printf(".puts(\"Hello, world\");<cr>\n\n");
-  
+  puts(helpstr);
 }
 
 void repl_expr(char*p){
@@ -225,21 +254,32 @@ void repl_load(char*p){
   pk_rebind( (sSym*)(U64)(((sSym*)(U64)SRCH_LIST)->data),"libc.so.6");
   //pk_rebind( ((sSym*)(U64)SRCH_LIST),"libc.so.6");
 }
+void repl_edit(char*p){
+  sSym* sym = pks_find_name((sSym*)(U64)SRCH_LIST,p);
+  FILE*f = fopen("sys/body.c","w");
+  if(sym){
+    src_to_file(sym->src,f);
+  }
+  fclose(f);
+  puts("edit and enter 'cc' when done\n");
+}
+    
 /*----------------------------------------------------------------------------
 
 ----------------------------------------------------------------------------*/
 
 void repl_loop(){
   pks_dump_protos();
-
+  puts("harn - an interactive C environment.  (c) 2022 StackSmith\n\n\
+      ...try 'help<cr>', or 'printf(\"Hello World!\\n\")<cr>'\n");
   while(1) {
     printf("> ");
     fgets(linebuf,1024,stdin);
 
     char*p=linebuf;
     U32 hash = cmd_hash(&p); // and advance to ws
-    printf("Hash: %08X, len %d\n",hash,(U32)(p-linebuf));
-    U32 cmdlen = (p-linebuf);
+    //    printf("Hash: %08X, len %d\n",hash,(U32)(p-linebuf));
+    //    U32 cmdlen = (p-linebuf);
     // if terminated by a (, compile line and execute as a function
     if('('==*p) { repl_expr(linebuf); continue ; }
 
@@ -248,13 +288,13 @@ void repl_loop(){
     { size_t len = strlen(linebuf);
       *(linebuf+len-1)=0;
     }
-
+    if(!strncmp("cc",linebuf,2))  { repl_compile(p);continue; }
     if(!strncmp("sys",linebuf,3)) { repl_sys(p);    continue; }
     // ox56299123
-    if(!strncmp("cc",linebuf,2))  { repl_compile(p);continue; }
     // 5CB7AA8A,
     if(!strncmp("words",linebuf,4)){ repl_words(p); continue; }
-    
+    if(!strncmp("edit",linebuf,4)){ repl_edit(p);  continue; }
+
     if(!strncmp("list",linebuf,4)){ repl_list(p);  continue; }
 
     if(!strncmp("help",linebuf,4)){ repl_help(p); continue; }
--- sys/body.c
+++ sys/body.c
@@ -1,25 +1,3 @@
-#define pass 4
-#if (pass==1)
-U32 ass = 1;
-#endif
-
-#if (pass==2)
-extern U32 ass;
-U32* p = &ass;
-#endif
- 
-#if (pass==3)
-U32 ass = 99;
-#endif 
-
-#if (pass==4)
-extern U32* p;
-void tester(){
-  printf("%d\n",*p);
+void fuck(){
+  printf("hello");
 }
-#endif
-
-
-
-
-
